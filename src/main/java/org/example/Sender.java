2729b5619be72a5e562bf19ad2455cf581cb5098
Zaur Khudiev <xudiyev_zaur@mail.ru>
2023-10-28 19:30:43 +0400

    Initial commit

---
+++ src/main/java/org/example/Sender.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.List;
+
+public class Sender {
+
+    private static final String USERNAME = "dist_user";
+    private static final String PASSWORD = "dist_pass_123";
+    private static final String SENDER_NAME = "Zaur";
+
+
+    public static void main(String[] args) {
+        List<String> dbIPs = List.of("192.168.1.1", "192.168.1.2");
+        
+
+
+
+    }
+}
