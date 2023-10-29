c1d65b4a3bdb208ecbf5a354810c0c9cb7fe6ced
Zaur Khudiev <xudiyev_zaur@mail.ru>
2023-10-29 17:34:31 +0400

    Commit Message

--- src/main/java/org/example/Reader.java
+++ src/main/java/org/example/Reader.java
@@ -1,16 +1,14 @@
 package org.example;
 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 
 public class Reader {
 
     private static final String USERNAME = "dist_user";
     private static final String PASSWORD = "dist_pass_123";
-    private static final String SENDER_NAME = "Zaur";
+    private static final String READER_NAME = "Zaur";
 
     public static void main(String[] args) {
 
@@ -22,15 +20,36 @@ public class Reader {
             new Thread(() -> {
                 while (true) {
                     try (Connection connection = DriverManager.getConnection("jdbc:postgresql://" + ip + ":5432/postgres", USERNAME, PASSWORD)) {
+                        String select = "SELECT id, SENDER_NAME, MESSAGE, SENT_TIME FROM ASYNC_MESSAGE WHERE RECEIVED_TIME IS NULL AND SENDER_NAME != ? LIMIT 1 FOR UPDATE";
 
+                        try (PreparedStatement preparedStatement = connection.prepareStatement(select)) {
+                            preparedStatement.setString(1, READER_NAME);
+                            ResultSet data = preparedStatement.executeQuery();
+                            if (data.next()) {
+                                System.out.printf("Sender " + data.getString("SENDER_NAME") + " sent " + data.getString("MESSAGE") + " at time " + data.getTimestamp("SENT_TIME"));
 
-                    } catch (SQLException exception) {
-                        throw new RuntimeException(exception);
+                                String update = "UPDATE ASYNC_MESSAGE SET RECEIVED_TIME = CURRENT_TIMESTAMP WHERE id = ?";
+                                try (PreparedStatement statementToUpdate = connection.prepareStatement(update)) {
+                                    statementToUpdate.setInt(1, data.getInt("id"));
+                                    int updated  = statementToUpdate.executeUpdate();
+
+                                    if (updated > 0) {
+                                        System.out.print("Message is set with Current time!");
+                                    } else {
+                                        System.out.print("Unable to set current time!");
+                                    }
+                                }
+
+                            }
+                        }
+
+                        } catch (SQLException e) {
+                            throw new RuntimeException(e);
+                        }
                     }
 
-                }
 
-            }
         }
     }
-    }
+}
+}
