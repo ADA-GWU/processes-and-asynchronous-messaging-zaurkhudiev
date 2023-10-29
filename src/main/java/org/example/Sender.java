e23f90a4d4a06bbb7760195a6e2cd80ae5de3b46
Zaur Khudiev <xudiyev_zaur@mail.ru>
2023-10-29 14:54:14 +0400

    Commit Message

--- src/main/java/org/example/Sender.java
+++ src/main/java/org/example/Sender.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,20 +15,48 @@ public class Sender {
 
 
     public static void main(String[] args) {
+
+        System.out.println("Hi");
+
         List<String> ipOfDatabases = List.of("192.168.1.1", "192.168.1.2");
-        List<Thread> threads = new ArrayList<>();
+        List<ThreadClass> threads = new ArrayList<>();
 
-
+        for (String ip : ipOfDatabases) {
+            ThreadClass threadClass = new ThreadClass(ip);
+            threads.add(threadClass);
+            new Thread(threadClass).start();
+        }
 
 
 
     }
 
-    public static class Thread implements Runnable {
+    public static class ThreadClass implements Runnable {
         private final String ipOfDatabase;
 
-        public Thread(String ipOfDatabase) {
+        public ThreadClass(String ipOfDatabase) {
             this.ipOfDatabase = ipOfDatabase;
+        }
+
+        public void send(String name, String message) {
+            try (Connection connection = DriverManager.getConnection("jdbc:postgresql://" + ipOfDatabase + ":5432/postgres", USERNAME, PASSWORD)) {
+                String insert = "INSERT INTO ASYNC_MESSAGE(SENDER_NAME, MESSAGE, SENT_TIME) VALUES(?, ?, CURRENT_TIMESTAMP)";
+                try (PreparedStatement s = connection.prepareStatement(insert)) {
+
+                    s.setString(1, name);
+                    s.setString(2, message);
+                    s.executeUpdate();
+
+
+                }
+                catch (SQLException exception) {
+                    exception.printStackTrace();
+                }
+
+            }
+            catch (SQLException exception) {
+                exception.printStackTrace();
+            }
         }
 
         @Override
